0fc96b2336ffd22f797338a1428c9bb41dc71ca9
Uratronc <malojouet35@gmail.com>
2023-12-22 08:57:53 +0100

    reorganisation

--- client/client.py
+++ client/client.py
@@ -3,7 +3,6 @@ import socket
 from threading import Thread
 import sys
 import json
-from tkinter import *
 
 # ========== Class ==========
 
@@ -70,70 +69,9 @@ class Client_tcp:
 
 
 
-##interface graphique
-class Ihm(Thread):
-    def __init__(self,root) -> None:
-        Thread.__init__(self)
-        self.__root = root
-        #setting title
-        root.title("RTphone_l_application_trop_bien.exe")
-        #setting w indow size
-        self.__width=600
-        self.__height=500
-        self.__screenwidth = root.winfo_screenwidth()
-        self.__screenheight = root.winfo_screenheight()
-        self.alignstr = '%dx%d+%d+%d' % (self.__width, self.__height, (self.__screenwidth - self.__width) / 2, (self.__screenheight - self.__height) / 2)
-        root.geometry(self.alignstr)
-        #creation des frames
-        self.__pourcent = self.pourcent(20, self.__height)
-        # navbar
-        self.__frame_navbar = Frame(root)
-        self.__frame_navbar.place(x=0, y=0, width=200, height=self.__height-self.__pourcent)
-        self.__frame_navbar.config(bg="purple")
-        # main
-        self.__frame_main = Frame(root)
-        self.__frame_main.place(x=200, y=0, width=self.__width-200, height=self.__height-self.__pourcent)
-        self.__frame_main.config(bg="yellow")
-        # footer
-        self.__frame_footer = Frame(root)
-        self.__frame_footer.place(x=0, y=self.__height-self.__pourcent, width=self.__width, height=self.__pourcent)
-        self.__frame_footer.config(bg="green")
-        self.__root.update()
-        height = self.__root.winfo_height()
-        width = self.__root.winfo_width() 
-        self.__pourcent = self.pourcent(20, height)
-        # navbar
-        self.__frame_navbar = Frame(self.__root)
-        self.__frame_navbar.place(x=0, y=0, width=width, height=height-self.__pourcent)
-        self.__frame_navbar.config(bg="purple")
-        # main
-        self.__frame_main = Frame(self.__root)
-        self.__frame_main.place(x=200, y=0, width=width-200, height=height-self.__pourcent)
-        self.__frame_main.config(bg="yellow")
-        # footer
-        self.__frame_footer = Frame(self.__root)
-        self.__frame_footer.place(x=0, y=height-self.__pourcent, width=width, height=self.__pourcent)
-        self.__frame_footer.config(bg="green")
-        
-
-    
-    def pourcent(self, pourcentage: int, taille: int)->int:
-        '''
-        pourcentage : pourcentage attendu
-        taille : taille dont on veut le pourcentage
-        '''
-        return int((taille*pourcentage)/100)
-
-        
-
-
 # ========== Main ==========
 def main():
-    root = Tk()
-    app = Ihm(root)
-    print(root.winfo_height())
-    root.mainloop()
-
+    pass
 
 
 if __name__ == '__main__':
