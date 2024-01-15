d876749d57b885a445cfb469c229267ab3409935
Ploc300 <ploc300@hotmail.com>
2024-01-15 16:00:30 +0100

    Add code to handle code '04' in auth function

--- client/client.py
+++ client/client.py
@@ -60,8 +60,10 @@ class Client_tcp:
             self.__reception_appel = reception_appel.reception(self.ip_serveur, 5003).start()
             self.__my_name = nom
             authentifier = True
+        elif code == '04':
+            authentifier = False
         else:
-            raise Exception(data)
+            print('Code error in auth func')
         return authentifier
     
     def get_phone(self, username: str)->str:
