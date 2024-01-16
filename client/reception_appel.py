548082bb02c76b65c3791ac4b2793fc37ae41fd4
Uratronc <malojouet35@gmail.com>
2024-01-16 12:41:42 +0100

    Add support for retrieving the caller's information in reception_appel.py

--- client/reception_appel.py
+++ client/reception_appel.py
@@ -1,6 +1,7 @@
 ## ========== Import ==========
 import socket, sys, json, appel_udp
 from threading import Thread
+from json import loads
 
 # ========== Class ==========
 
@@ -11,6 +12,7 @@ class reception():
             self.__port_serveur = port_serveur
             self.__port_client = 5003
             self.__appell : appel_udp = None
+            self.__who_call : str = None
             self.__connexion = True
             self.__appeller = False
             self.__accepter = False
@@ -19,7 +21,9 @@ class reception():
             self.__socket_echange.bind(('', self.__port_client))
             while self.__connexion:
                 data, addr = self.__socket_echange.recvfrom(512)
-                if data.decode('utf-8') == '14':
+                code,data = data.decode('utf-8').split(' ',1)
+                if code == '14':
+                    self.__who_call = loads(data)
                     if not self.__appeller:
                         if self.__accepter:
                             self.__socket_echange.sendto(b'15', (self.__ip_serveur, self.__port_serveur))
@@ -44,7 +48,7 @@ class reception():
             self.__appell = None
         
         def get_who_call(self)->str:
-            return self.__appell.get_who_call()
+            return self.__who_call
         
         def stop(self)->None:
             self.__connexion = False
